1848a8ecfc663954d32842c35a2ff6905596559f
Shanthanu Bhardwaj <sbhardwaj@palantir.com>
2015-08-14 18:06:56 -0700

    Initial Commit

---
+++ multilingual.py
@@ -0,0 +1,39 @@
+
+#//                    C ONLY              PYTHON ONLY              BOTH
+
+# define MULTILINE(a) #a
+# define print(a) printf("%s",a) /*             
+print "HAHA"                                    # Python ONLY
+mydict={}                                            # Python ONLY
+mydict["Hello"]='World'                              # Python ONLY
+""" */                                           
+                    main() {                    // C only
+                     char c, i;                 // C only And STRING BEGINS
+                     char* Z=MULTILINE(
+#/* 
+                                                Neither
+"""    #*/           
+#//                                             #<Both>
+mydict["Hello"] = "World";              
+mydict["Good"] = "Grief";              
+mydict["party"] = "hurrah";              
+mydict["food"] = "ewwwww";              
+Z = mydict["party"];                             
+ 
+#/*
+"""*/ 
+);                                              // C STRING ENDS
+//"""
+
+print(Z); 
+#//                                             #</Both>
+
+
+# define O2 /*             
+print "PYTHON ONLY"                             # Python ONLY
+""" */                  
+                    };                         // C only
+#define C2 /*            
+"""    #*/           
+
+
